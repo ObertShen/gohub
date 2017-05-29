8492d35fce5d6f481383d29584b293112aa816ee
Obert <xds5@qq.com>
2017-05-29 11:38:50 +0800

    Bugfix: multi repos or branches trigger failed

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -10,19 +11,23 @@ import (
 	"os/exec"
 )
 
+// Repository git 仓库名称
 type Repository struct {
-	Name string
+	Name string `json:"name"`
 }
 
-type GithubJson struct {
-	Repository Repository
-	Ref        string
+// GithubJSON github 发送的webhooks 中的结构
+type GithubJSON struct {
+	Repository Repository `json:"repository"`
+	Ref        string     `json:"ref"`
 }
 
+// Config 存放多个仓库配置
 type Config struct {
 	Hooks []Hook
 }
 
+// Hook 对应 hooks.json 中的配置
 type Hook struct {
 	Repo   string
 	Branch string
@@ -35,10 +40,12 @@ func loadConfig(configFile *string) {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	err = json.Unmarshal(configData, &config)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	for i := 0; i < len(config.Hooks); i++ {
 		addHandler(config.Hooks[i].Repo, config.Hooks[i].Branch, config.Hooks[i].Shell)
 	}
@@ -49,26 +56,28 @@ func setLog(logFile *string) {
 	if err != nil {
 		panic("cannot write log")
 	}
+
 	log.SetOutput(log_handler)
 	log.SetFlags(5)
 }
 
 func startWebserver() {
-	log.Println("starting webserver")
+	log.Println("starting webserver on port: " + *port)
 	http.ListenAndServe(":"+*port, nil)
 }
 
 func addHandler(repo, branch, shell string) {
 	uri := branch
 	branch = "refs/heads/" + branch
-	http.HandleFunc("/"+repo+"_"+uri, func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/"+repo+"/"+uri, func(w http.ResponseWriter, r *http.Request) {
 		decoder := json.NewDecoder(r.Body)
-		var data GithubJson
+		var data GithubJSON
 		err := decoder.Decode(&data)
 
 		if err != nil {
 			log.Println(err)
 		}
+
 		if data.Repository.Name == repo && data.Ref == branch {
 			executeShell(shell)
 		}
@@ -76,25 +85,30 @@ func addHandler(repo, branch, shell string) {
 }
 
 func executeShell(shell string) {
+	fmt.Println(shell)
 	out, err := exec.Command(shell).Output()
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	log.Printf("Shell output was: %s\n", out)
 }
 
 var (
-	port       = flag.String("port", "7654", "port to listen on")
-	configFile = flag.String("config", "./config.json", "config")
-	logFile    = flag.String("log", "./log", "log file")
+	// Set port number
+	port = flag.String("port", "7654", "port to listen on")
+	// Set config path
+	configFile = flag.String("config", "./example.json", "config")
+	// Set log path
+	logFile = flag.String("log", "./log", "log file")
 )
 
 func init() {
 	flag.Parse()
+	setLog(logFile)
+	loadConfig(configFile)
 }
 
 func main() {
-	setLog(logFile)
-	loadConfig(configFile)
 	startWebserver()
 }
